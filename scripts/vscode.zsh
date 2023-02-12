d352793be8a8fd4ab7e51a04de184958e4d744b8
Philipp Muens <philipp@muens.io>
2023-02-12 19:15:58 +0100

    Install and configure Visual Studio Code

---
+++ scripts/vscode.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+echo ""
+echo "--- Starting Visual Studio Code Setup ---"
+
+echo "Installing Visual Studio Code Extensions..."
+echo ""
+cat "$DOTFILES/vscode/extensions" | xargs -L 1 code --install-extension
