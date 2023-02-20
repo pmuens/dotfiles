80b394af651aed285a5d152138d98d5dac8241cc
Philipp Muens <philipp@muens.io>
2023-02-20 20:03:58 +0100

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,63 @@
 #!/usr/bin/env zsh
 
-alias rm=trash
-alias lls='exa -laFh --git'
+# Replacements
+alias ls=exa
+alias grep=rg
+alias rm='trash'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Shorthands
+alias c='clear'
+alias l='exa -laFh --git'
+alias s="source $HOME/.zshrc"
+alias g='lazygit'
+alias j='z'
+alias v='nvim'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias dt="cd $HOME/Desktop"
+alias dw="cd $HOME/Downloads"
+alias co="cd $HOME/code"
+
+
+# System Management
+alias mb='mackup backup'
+alias mr='mackup restore'
 alias bbd="brew bundle dump --file $DOTFILES/homebrew/Brewfile --force --describe"
+alias ug="$DOTFILES/install"
+
+# Development
+alias da='direnv allow'
+alias ai='asdf install'
+
+# Git
+alias gc='git commit'
+alias gco='git checkout'
+alias ga='git add'
+alias gp='git pull'
+alias gf='git fetch'
+alias gfp='git fetch --prune'
+alias grc='git rebase --continue'
+alias gra='git rebase --abort'
+
+# Utils
+alias trail='<<<${(F)path}'
+alias ftrail='<<<${(F)fpath}'
+alias trim="awk '{\$1=\$1;print}'"
+alias jj='pbpaste | jsonpp | pbcopy'
+alias serve='python3 -m http.server'
+
+# Misc
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+alias pubkey="cat $PUBKEY_FILE_PATH | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+function mkcd() {
+  mkdir -p "$@" && cd "$_";
+}
+
+function note() {
+  echo "date: $(date)" >> $HOME/drafts.txt
+  echo "$@" >> $HOME/drafts.txt
+  echo "" >> $HOME/drafts.txt
+}
