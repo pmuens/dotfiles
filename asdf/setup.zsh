df92ed0b6a38ce45fc24364225d703290f86666b
Philipp Muens <philipp@muens.io>
2023-03-01 20:24:03 +0100

    Add repository URLs to asdf plugin installations

--- asdf/setup.zsh
+++ asdf/setup.zsh
@@ -3,8 +3,8 @@
 echo ""
 echo "--- Starting asdf Setup ---"
 
-suppress_error asdf plugin add direnv
+suppress_error asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git
 suppress_error asdf install direnv latest
 suppress_error asdf global direnv latest
 
-suppress_error asdf plugin add nodejs
+suppress_error asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
