104d2904ccb63a3ec9afa4b0c1a2c39314caf4bf
Philipp Muens <philipp@muens.io>
2023-10-13 10:42:08 +0200

    Switch to different asdf rust plugin

--- asdf/setup.zsh
+++ asdf/setup.zsh
@@ -6,7 +6,7 @@ echo "--- Starting asdf Setup ---"
 suppress_error asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git
 suppress_error asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 suppress_error asdf plugin add python https://github.com/asdf-community/asdf-python.git
-suppress_error asdf plugin add rust https://github.com/asdf-community/asdf-rust.git
+suppress_error asdf plugin add rust https://github.com/code-lever/asdf-rust.git
 suppress_error asdf plugin-add zig https://github.com/asdf-community/asdf-zig.git
 
 suppress_error asdf install
