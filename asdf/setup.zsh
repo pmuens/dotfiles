429e884334c11c1e0ec13310b98105cabe49cda9
Philipp Muens <philipp@muens.io>
2023-12-01 13:54:14 +0100

    Install asdf bun plugin

--- asdf/setup.zsh
+++ asdf/setup.zsh
@@ -3,6 +3,7 @@
 echo ""
 echo "--- Starting asdf Setup ---"
 
+suppress_error asdf plugin add bun https://github.com/cometkim/asdf-bun.git
 suppress_error asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git
 suppress_error asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 suppress_error asdf plugin add python https://github.com/asdf-community/asdf-python.git
