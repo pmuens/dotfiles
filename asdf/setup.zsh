e4da6c2b86c6097283a7e64c5bcaccd1b5cab6ab
Philipp Muens <philipp@muens.io>
2024-08-13 15:18:21 +0200

    Install asdf make plugin

--- asdf/setup.zsh
+++ asdf/setup.zsh
@@ -7,6 +7,7 @@ suppress_error asdf plugin add bun https://github.com/cometkim/asdf-bun.git
 suppress_error asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
 suppress_error asdf plugin add direnv https://github.com/asdf-community/asdf-direnv.git
 suppress_error asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+suppress_error asdf plugin add make https://github.com/yacchi/asdf-make.git
 suppress_error asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 suppress_error asdf plugin add python https://github.com/asdf-community/asdf-python.git
 suppress_error asdf plugin add rust https://github.com/code-lever/asdf-rust.git
