7ed75e3b6afdec8950552875ea0d2576c5d42c87
Philipp Muens <philipp@muens.io>
2026-08-11 12:59:47 +0200

    Remove deprecated describe option

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -24,7 +24,7 @@ alias co="cd $CODE"
 # System Management
 alias mb='mackup backup'
 alias mr='mackup restore'
-alias bbd="brew bundle dump --file $DOTFILES/homebrew/Brewfile --force --describe --no-vscode"
+alias bbd="brew bundle dump --file $DOTFILES/homebrew/Brewfile --force --no-vscode"
 alias ug="$DOTFILES/install"
 
 # Development
